dc953a8d0ec8dbd3a6268baa8b8b4f2079df885f
Tristan Lotivio <tclotivio@outlook.com>
2021-04-24 17:23:55 -0400

    API usage in Next

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,12 +6,8 @@ const Navbar = () => {
         <div>
             <ul className={styles.list}>
                 <li className={styles.listItem}><Link href="/"><a className={styles.text}>Home</a></Link></li>
-                <li className={styles.listItem}><Link href="/about"><a className={styles.text}>About</a></Link></li>
+                <li className={styles.listItem}><Link href="/about"><a className={styles.text}>Second Page</a></Link></li>
             </ul>
-
-            <style jsx>{`
-                
-            `}</style>
         </div>
     );
 }
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,35 @@
 import Layout from '../components/layout';
 
+import {useState, useEffect} from 'react';
+
 const About = () => {
+
+    const [apiInfo, setApiInfo] = useState(0);
+
+    useEffect(async(e) => {
+        await fetch('/api/hello')
+        .then(promise=>promise.json())
+        .then(data=>{
+            setApiInfo(data)
+        })
+        .catch(
+            error=>{
+                console.log(error)
+            }
+        )
+
+        document.getElementById("api").innerHTML = apiInfo.name
+    })
+
     return(
         <Layout>
             <div>
                 <h1>
-                    About NextJS
+                    Second Page
                 </h1>
                 <p>Application to Demonstrate NextJS Basics</p>
+                <div id ="api">
+                </div>
             </div>
         </Layout>
     );
